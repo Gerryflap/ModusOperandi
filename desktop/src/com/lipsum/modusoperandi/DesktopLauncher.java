333697c0452b8cca813d0e1025580f02e5c42e60
gerryflap <gerry2211@gmail.com>
2023-12-01 21:33:48 +0100

    Initial commit

---
+++ desktop/src/com/lipsum/modusoperandi/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.lipsum.modusoperandi;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.lipsum.modusoperandi.Game;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("ModusOperandi");
+		new Lwjgl3Application(new Game(), config);
+	}
+}
